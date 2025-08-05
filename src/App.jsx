152c8342e2d20c1ab8e10eee8a316546b798aaca
jose angel pacheco belisario <joseangelpb@gmail.com>
2025-08-05 18:24:39 -0400

    Se creo el ProductRegister correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ProductRegister from './pages/ProductRegister/ProductRegister';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/ProductRegister" element={<ProductRegister/>} />
         {/* <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
