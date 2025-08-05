0c487d5eda68fe1f289629f00b0bef04f6eb7385
Oscar <olbo.1995@gmail.com>
2025-08-04 23:12:52 -0400

    add back

---
+++ Backend/src/models/users.model.js
@@ -0,0 +1,7 @@
+import format from 'pg-format'
+import pool from '../../db/config.js'
+import bcrypt from 'bcryptjs'
+
+export const createUserModel = async (email, password, rol, lenguage) => {
+
+}
