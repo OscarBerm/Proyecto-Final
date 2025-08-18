c363c164a69ec5e13be8429df511c52e51f4a896
Cristoffer Venega DLC <cristoffer.venega@gmail.com>
2025-08-18 00:15:12 -0400

    authController (loginUser), env.local(add jwtExpires)

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,9 +1,29 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import 'dotenv/config'
+import { getUserByEmailModel } from "../models/users.model";
 
-const loginUser = async (req, res) => {
-    
+export const loginUser = async (req, res) => {
+    try{
+        const {email, password} = req.body || {};
+        
+        if(!email || !password){
+            return res.status(400).send("Debes ingresar tu Direccion de correo electronico y contraseña");
+        }
+
+        const user = await getUserByEmailModel(email);
+        
+        if(!user){
+            return res.status(400).send("Error, Usuario o Contraseña incorrecta.")
+        }
+        if(!bcrypt.compareSync(password, user.password)){
+            return res.status(400).send("Error, Usuario o Contraseña incorrecta.")            
+        }
+
+        const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRATION});
+        return res.status(200).json(token);
+    }catch(e){
+        return res.status(500).json({Error: e.Error, Mensaje: e.message})
+    }
 }
 
-export { loginUser }
