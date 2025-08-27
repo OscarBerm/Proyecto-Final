f6480990f551661c9bef651992eee227235402cf
Oscar <olbo.1995@gmail.com>
2025-08-27 05:00:50 -0400

    arreglado error en una variable

--- Backend/middleware/isAdmin.js
+++ Backend/middleware/isAdmin.js
@@ -1,7 +1,7 @@
 export const isAdmin = (req, res, next) => {
   const user = req.user
 
-  if (user && user.role === 'admin') {
+  if (user && user.rol === 'admin') {
     next()
   } else {
     res.status(403).json({ error: 'Acceso denegado: Se requieren permisos de administrador.' })
